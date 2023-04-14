cbd47ede64fd12b8285de32f2c582ee01e70d03a
koomchang <leahpar0401@gmail.com>
2023-04-14 15:35:40 +0900

    add: 백준 1008

--- BaekJoon/BronzeV/_1008/Main.java
+++ BaekJoon/BronzeV/_1008/Main.java
@@ -1,2 +1,12 @@
-package BaekJoon.BronzeV._1008;public class Main {
+package BaekJoon.BronzeV._1008;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(a / b);
+    }
 }
