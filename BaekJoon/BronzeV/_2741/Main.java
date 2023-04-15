7b985fb7dc0777c76c1c56aad461d3ae6dbddc98
koomchang <leahpar0401@gmail.com>
2023-04-15 22:14:53 +0900

    add: 백준 2741

---
+++ BaekJoon/BronzeV/_2741/Main.java
@@ -0,0 +1,13 @@
+package BaekJoon.BronzeV._2741;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            System.out.println(i);
+        }
+    }
+}
