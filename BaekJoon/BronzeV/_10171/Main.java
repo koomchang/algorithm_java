46332db3a20ba6d6cfd44e1558eb8c2d9e54ec18
koomchang <leahpar0401@gmail.com>
2023-04-15 22:24:56 +0900

    add: 백준 10171

---
+++ BaekJoon/BronzeV/_10171/Main.java
@@ -0,0 +1,11 @@
+package BaekJoon.BronzeV._10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
