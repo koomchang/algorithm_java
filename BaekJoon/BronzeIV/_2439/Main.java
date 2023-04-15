83ce9fb8f69580720c8e3adebffe2818fac685d7
koomchang <leahpar0401@gmail.com>
2023-04-16 00:07:28 +0900

    add: 백준 2439

---
+++ BaekJoon/BronzeIV/_2439/Main.java
@@ -0,0 +1,19 @@
+package BaekJoon.BronzeIV._2439;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
