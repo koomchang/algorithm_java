8ac8d5a694a0a7df33df8fdeed24dd2c16379b6d
koomchang <leahpar0401@gmail.com>
2023-05-02 14:01:49 +0900

    add: 백준 10926

---
+++ BaekJoon/BronzeV/_10926/Main.java
@@ -0,0 +1,13 @@
+package BaekJoon.BronzeV._10926;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder word = new StringBuilder(sc.nextLine());
+        word.append("??!");
+        System.out.println(word);
+    }
+}
