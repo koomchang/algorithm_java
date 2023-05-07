29430c375854c498e4a975d43c0b2516c7983b37
koomchang <leahpar0401@gmail.com>
2023-05-08 02:49:15 +0900

    add: 백준 5337

--- BaekJoon/SilverII/_11053/Main.java
+++ BaekJoon/SilverII/_11053/Main.java
@@ -1,2 +1,23 @@
-package BaekJoon.SilverII._11053;public class Main {
+package BaekJoon.SilverII._11053;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+//        System.out.println(main.solution(N, arr));
+    }
+
+//    private int solution(int N, int[] arr) {
+//        int [] seq = new int[]
+//
+//    }
+
 }
