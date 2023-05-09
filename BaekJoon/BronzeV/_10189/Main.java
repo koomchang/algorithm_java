df63cc3bb439e87389d64bb804c1659768eeb122
koomchang <leahpar0401@gmail.com>
2023-05-09 16:52:39 +0900

    add: 백준 10189

---
+++ BaekJoon/BronzeV/_10189/Main.java
@@ -0,0 +1,11 @@
+package BaekJoon.BronzeV._10189;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+    }
+}
