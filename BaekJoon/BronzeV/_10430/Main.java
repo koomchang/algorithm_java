fdab439cc071cf80070743823fc61417cc044732
koomchang <leahpar0401@gmail.com>
2023-11-20 21:27:58 +0900

    add: 백준 10430

---
+++ BaekJoon/BronzeV/_10430/Main.java
@@ -0,0 +1,20 @@
+package BaekJoon.BronzeV._10430;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int A = Integer.parseInt(split[0]);
+        int B = Integer.parseInt(split[1]);
+        int C = Integer.parseInt(split[2]);
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
+    }
+}
