668d4dbee3f30a2e55bcad11f70638702a59bc60
koomchang <leahpar0401@gmail.com>
2023-11-20 21:24:36 +0900

    add: 백준 18108

---
+++ BaekJoon/BronzeV/_18108/Main.java
@@ -0,0 +1,15 @@
+package BaekJoon.BronzeV._18108;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        num -= 543;
+        System.out.println(num);
+    }
+}
