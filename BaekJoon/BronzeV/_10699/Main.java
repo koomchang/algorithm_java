3a98ef4c50b5b0ec6a9a0ed87aed8efc84044ddf
koomchang <leahpar0401@gmail.com>
2023-11-22 23:55:45 +0900

    add: 백준 10699

---
+++ BaekJoon/BronzeV/_10699/Main.java
@@ -0,0 +1,13 @@
+package BaekJoon.BronzeV._10699;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        System.out.println(format.format(date));
+    }
+}
