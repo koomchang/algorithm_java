f159a8159d59eaec49de5d8245ea4a5b8d528b96
koomchang <leahpar0401@gmail.com>
2023-11-25 23:31:20 +0900

    add: 백준 5339

---
+++ BaekJoon/BronzeV/_5339/Main.java
@@ -0,0 +1,18 @@
+package BaekJoon.BronzeV._5339;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("""
+                     /~\\
+                    ( oo|
+                    _\\=/_
+                   /  _  \\
+                  //|/.\\|\\\\
+                 ||  \\ /  ||
+                ============
+                |          |
+                |          |
+                |          |""");
+    }
+}
