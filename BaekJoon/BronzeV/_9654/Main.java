8a3f794c444f37b1b53e19b3611d8fbe098136d6
koomchang <leahpar0401@gmail.com>
2023-11-26 22:39:58 +0900

    add: 백준 9654

---
+++ BaekJoon/BronzeV/_9654/Main.java
@@ -0,0 +1,14 @@
+package BaekJoon.BronzeV._9654;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("""
+                SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+                N2 Bomber      Heavy Fighter  Limited    21       \s
+                J-Type 327     Light Combat   Unlimited  1        \s
+                NX Cruiser     Medium Fighter Limited    18       \s
+                N1 Starfighter Medium Fighter Unlimited  25       \s
+                Royal Cruiser  Light Combat   Limited    4        \s""");
+    }
+}
