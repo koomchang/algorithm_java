1af9b93a9413ee627d198f26bc33facce538c124
koomchang <leahpar0401@gmail.com>
2023-12-04 02:02:17 +0900

    add: 백준 15680

---
+++ BaekJoon/BronzeV/_15680/Main.java
@@ -0,0 +1,17 @@
+package BaekJoon.BronzeV._15680;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        if (i == 0) {
+            System.out.println("YONSEI");
+            return;
+        }
+        System.out.println("Leading the Way to the Future");
+    }
+}
