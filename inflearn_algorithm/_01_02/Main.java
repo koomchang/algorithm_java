6e8b31aeda48c657af0fc3be46a910d8d1a3b280
koomchang <leahpar0401@gmail.com>
2024-01-08 00:13:31 +0900

    add: 인프런 01-02 대소문자 변환

---
+++ inflearn_algorithm/_01_02/Main.java
@@ -0,0 +1,27 @@
+package inflearn_algorithm._01_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(solution(s));
+    }
+
+    private static String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c >= 65 && c <= 90) {
+                c += 32;
+            } else {
+                c -= 32;
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
