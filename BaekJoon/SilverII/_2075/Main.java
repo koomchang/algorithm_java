cbecbb8cae55f12a36c5ad37736c7194004e88c5
koomchang <leahpar0401@gmail.com>
2024-03-05 16:27:50 +0900

    add: 백준 2075

--- BaekJoon/SilverII/_2075/Main.java
+++ BaekJoon/SilverII/_2075/Main.java
@@ -1,2 +1,32 @@
-package BaekJoon.SilverII._2075;public class Main {
+package BaekJoon.SilverII._2075;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(2_250_000);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            queue.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                if (queue.peek() < tmp) {
+                    queue.poll();
+                    queue.add(tmp);
+                }
+            }
+        }
+        System.out.println(queue.poll());
+    }
 }
