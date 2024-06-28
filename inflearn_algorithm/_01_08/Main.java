9b45ae04e196dd014ee5b519e9adc8ceb89e329b
koomchang <leahpar0401@gmail.com>
2024-06-28 21:19:29 +0900

    fix: 인프런 01-08 유효한 펠린드롬 정규표현식 이용

--- inflearn_algorithm/_01_08/Main.java
+++ inflearn_algorithm/_01_08/Main.java
@@ -12,22 +12,29 @@ class Main {
     }
 
     static String solution(String input) {
-        String answer = "YES";
-        int lt = 0, rt = input.length() - 1;
-        char[] arr = input.toUpperCase().toCharArray();
-        while (lt < rt) {
-            if (!Character.isAlphabetic(arr[lt])) {
-                lt++;
-            } else if (!Character.isAlphabetic(arr[rt])) {
-                rt--;
-            } else {
-                if (arr[lt] != arr[rt]) {
-                    return "NO";
-                } else {
-                    lt++;
-                    rt--;
-                }
-            }
+//        String answer = "YES";
+//        int lt = 0, rt = input.length() - 1;
+//        char[] arr = input.toUpperCase().toCharArray();
+//        while (lt < rt) {
+//            if (!Character.isAlphabetic(arr[lt])) {
+//                lt++;
+//            } else if (!Character.isAlphabetic(arr[rt])) {
+//                rt--;
+//            } else {
+//                if (arr[lt] != arr[rt]) {
+//                    return "NO";
+//                } else {
+//                    lt++;
+//                    rt--;
+//                }
+//            }
+//        }
+//        return answer;
+        String answer = "NO";
+        input = input.toUpperCase().replaceAll("[^A-Z]", "");
+        String tmp = new StringBuilder(input).reverse().toString();
+        if (input.equals(tmp)) {
+            return "YES";
         }
         return answer;
     }
