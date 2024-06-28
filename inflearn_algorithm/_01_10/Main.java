c1da987c5409df112c6ebc12b296d5ca44abe9f6
koomchang <leahpar0401@gmail.com>
2024-06-28 21:54:41 +0900

    add: 인프런 01-10 가장 짧은 문자거리

---
+++ inflearn_algorithm/_01_10/Main.java
@@ -0,0 +1,44 @@
+package inflearn_algorithm._01_10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        System.out.println(solution(input));
+    }
+
+    static String solution(String[] input) {
+        char[] s = input[0].toCharArray();
+        char t = input[1].charAt(0);
+        int[] answer = new int[s.length];
+        int tmp = 1000;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] == t) {
+                tmp = 0;
+            } else {
+                tmp++;
+            }
+            answer[i] = tmp;
+        }
+        tmp = 1000;
+        for (int i = s.length - 1; i >= 0; i--) {
+            if (s[i] == t) {
+                tmp = 0;
+            } else {
+                tmp++;
+            }
+            if (answer[i] > tmp) {
+                answer[i] = tmp;
+            }
+        }
+        StringBuilder str = new StringBuilder();
+        for (int i : answer) {
+            str.append(i).append(" ");
+        }
+        return str.toString();
+    }
+}
