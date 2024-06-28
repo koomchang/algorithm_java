ff3ae6ee24746ae833fdee1ef723dea66585e4f2
koomchang <leahpar0401@gmail.com>
2024-06-28 21:11:41 +0900

    add: 인프런 01-08 유효한 펠린드롬

--- inflearn_algorithm/_01_08/Main.java
+++ inflearn_algorithm/_01_08/Main.java
@@ -1,2 +1,34 @@
-package inflearn_algorithm._01_08;public class Main {
+package inflearn_algorithm._01_08;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(solution(input));
+    }
+
+    static String solution(String input) {
+        String answer = "YES";
+        int lt = 0, rt = input.length() - 1;
+        char[] arr = input.toUpperCase().toCharArray();
+        while (lt < rt) {
+            if (!Character.isAlphabetic(arr[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(arr[rt])) {
+                rt--;
+            } else {
+                if (arr[lt] != arr[rt]) {
+                    return "NO";
+                } else {
+                    lt++;
+                    rt--;
+                }
+            }
+        }
+        return answer;
+    }
 }
