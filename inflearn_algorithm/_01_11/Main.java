9ea8713ec1885723a01f7198d8fd5ebc1a1efc8b
koomchang <leahpar0401@gmail.com>
2024-06-28 22:14:10 +0900

    add: 인프런 01-11 문자열 압축

---
+++ inflearn_algorithm/_01_11/Main.java
@@ -0,0 +1,31 @@
+package inflearn_algorithm._01_11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(solution(input));
+    }
+
+    static String solution(String input) {
+        input += " ";
+        int tmp = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (input.charAt(i) == input.charAt(i + 1)) {
+                tmp++;
+            } else {
+                sb.append(input.charAt(i));
+                if (tmp != 1) {
+                    sb.append(tmp);
+                }
+                tmp = 1;
+            }
+        }
+        return sb.toString();
+    }
+}
