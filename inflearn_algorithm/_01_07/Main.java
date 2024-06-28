0a75281092e6cca93e4a3ee56f17186db8b0924e
koomchang <leahpar0401@gmail.com>
2024-06-28 21:11:17 +0900

    add: 인프런 01-07 회문 문자열

---
+++ inflearn_algorithm/_01_07/Main.java
@@ -0,0 +1,24 @@
+package inflearn_algorithm._01_07;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(solution(input));
+    }
+
+    static String solution(String input) {
+        String answer = "YES";
+        input = input.toUpperCase();
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+                return "NO";
+            }
+        }
+        return answer;
+    }
+}
---
+++ inflearn_algorithm/_01_08/Main.java
@@ -0,0 +1,2 @@
+package inflearn_algorithm._01_08;public class Main {
+}
