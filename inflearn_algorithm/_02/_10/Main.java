344c58fe7e0badc60575d2b115e99adbdc652c44
koomchang <leahpar0401@gmail.com>
2024-07-09 16:40:28 +0900

    add: 인프런 02-10 봉우리

---
+++ inflearn_algorithm/_02/_10/Main.java
@@ -0,0 +1,43 @@
+package inflearn_algorithm._02._10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+        System.out.println(solution(n, arr));
+    }
+
+    static int solution(int n, int[][] arr) {
+        int answer = 0;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 && nx < n && ny < n && ny >= 0 && arr[i][j] <= arr[nx][ny]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
